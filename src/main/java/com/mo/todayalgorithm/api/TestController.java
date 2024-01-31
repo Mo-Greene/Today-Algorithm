00e5ca3db35c44819af6f76aadccb7bd09fad025
MoGreene <13blueboy13@naver.com>
2024-01-31 15:18:30 +0900

    feat: jenkins health check!

---
+++ src/main/java/com/mo/todayalgorithm/api/TestController.java
@@ -0,0 +1,24 @@
+package com.mo.todayalgorithm.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.mo.todayalgorithm.api
+ * fileName       : TestController
+ * author         : Jang Hyun Suk
+ * date           : 2024-01-31
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-01-31      Jang Hyun Suk       최초 생성
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("")
+    public String versionCheck() {
+        return "Jenkins Test : Version 0.0.1";
+    }
+}
